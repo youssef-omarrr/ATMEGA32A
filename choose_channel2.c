dd6f5e1ce1673ed46031517e70c51543d0743f89
Youssef Omar the first <es-youssef.moh.ah2026@alexu.edu.eg>
2024-08-23 22:39:01 +0300

    another way for the choose channel project

---
+++ choose_channel2.c
@@ -0,0 +1,96 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <string.h>
+#include<avr/interrupt.h>
+
+#include "DIO.h"
+#include "LEDs.h"
+#include "buttons.h"
+#include "outdev.h"
+//#include "mLCD.h"
+#include "mLCD4.h"
+#include "keypad.h"
+#include "intr.h"
+#include "mADC.h"
+
+char x;
+
+ISR(ADC_vect) {
+    LCD4_data_num(ADC);
+    _delay_ms(1000);
+    LCD4_CLEAR();
+}
+
+int main() {
+    initLCD4();
+    initKEYPAD();
+    LCD4_CLEAR();
+    
+    // ADC
+    initADC (0,AVCC, ADC_freq_128);
+    
+    //global interrupt enable
+        sei();
+
+    //pooling //same idea as delay
+        //checkFlag; //checks if flag is one and keeps looping until it is one
+
+    while (1) {        
+        LCD4_goto(0, 0);
+        LCD4_data_str("Choose (1,2,3)");
+        LCD4_goto(1, 0);
+        
+        char x = readkey();
+        LCD4_data(x);
+        _delay_ms(200);
+        LCD4_goto(0, 0);
+        
+        //select channel according to button pressed
+        if (x == '1'){
+            LCD4_CLEAR();
+            LCD4_data_str("Channel 1 data:");
+            LCD4_goto(1, 0);
+            
+            ADC_select_ch(0);
+            _delay_ms(50);
+            ADC_startConv(); //when the ISR finishes the LCD gets overwritten data from before the interrupt 
+            _delay_ms(50);
+            initLCD4(); //so we re initialize the LCD to reset the enable and data
+            _delay_ms(50);
+            
+        }
+        else if (x == '2'){
+            LCD4_CLEAR();
+            LCD4_data_str("Channel 2 data:");
+            LCD4_goto(1, 0);
+            
+            ADC_select_ch(1);
+            _delay_us(50);
+            ADC_startConv();
+            _delay_ms(50);
+            initLCD4(); 
+            _delay_ms(50);
+        }
+        else if (x == '3'){
+            LCD4_CLEAR();
+            LCD4_data_str("Channel 3 data:");
+            LCD4_goto(1, 0);
+            
+            ADC_select_ch(2);
+            _delay_us(50);
+            ADC_startConv();
+            _delay_ms(50);
+            initLCD4();
+            _delay_ms(50);
+        }
+        else {
+            LCD4_CLEAR();
+            LCD4_data_str("Wrong input!");
+            _delay_ms (500);
+            LCD4_CLEAR();
+        }
+    }
+
+    return 0;
+}
