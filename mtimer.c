6cb5e3bf9d89706a474b5e1d7fe0b7448a104dbb
Youssef Omar the first <es-youssef.moh.ah2026@alexu.edu.eg>
2024-09-06 02:12:40 +0300

    added timer 1 and asynch in timer 2

--- mtimer.c
+++ mtimer.c
@@ -2,7 +2,9 @@
 #include "mtimer.h"
 #include "DIO.h"
 
+/////////////////////////////////////////////////////////////////////////
 ////////////////////////////timer0///////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 /**
  * 
  * @param wave TIMER_NORMAL      
@@ -98,7 +100,9 @@ void timer0_prescaler_reset(){
     SFIOR |= (1<<PSR10); //When this bit is written to one, the Timer/Counter1 and Timer/Counter0 prescaler will be reset. The bit will be
                          //cleared by hardware after the operation is performed.
 }
+/////////////////////////////////////////////////////////////////////////
 ////////////////////////////timer2///////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 /**
  * 
  * @param wave TIMER_NORMAL      
@@ -166,7 +170,7 @@ void timer2_COMP_int_en() {
 }
 
 void timer2_OC2_init(){ //in the main we should init this first before the timer
-    setPINB_dir(OC2, OUT); //make sure we set it to out put first;
+    setPINB_dir(OC2, OUT); //make sure we set it to output first;
 }
 
 /**
@@ -193,9 +197,17 @@ void timer2_OC2_selectMode(char mode){
 /**
  * 
  * @param mode clk_io input/output clock
- *             clk_osc oscillator clock
+ *             clk_osc oscillator clock (Asynch)
  */
 void timer2_asynch(char mode){ //choose which clock to use
+    
+    char x,y,z;
+    x = TCNT2;
+    y = OCR2;
+    z = TCCR2;
+    
+    //Configure TOSC1 (PB6) and TOSC2 (PB7) as input pins for the crystal
+    DDRB &= ~((1 << PB6) | (1 << PB7)); // Set TOSC1 and TOSC2 as input
     
     // 1) clear interrupts enables
     TIMSK &= ~(1<<TOIE2 | 1<<OCIE2); 
@@ -210,13 +222,221 @@ void timer2_asynch(char mode){ //choose which clock to use
     }
     
     // 3) write new values for TCNT2, OCR2, TCCR2
-    // 4) wait for TCN2UB, OCR2UB, TCR2UB
+    TCNT2 = x;
+    OCR2  = y;
+    TCCR2 = z;
+    
+    // 4) wait for TCN2UB, OCR2UB, TCR2UB to synchronize
+    while (ASSR & ((1 << TCR2UB) | (1 << OCR2UB) | (1 << TCN2UB)));
+    
     // 5) clear interrupt flags
     TIFR &= ~(1<<OCF2 | 1<<TOV2);
     
     // 6) enable interrupt, if needed
+    //timer2_TOV_int_en();
+    //timer2_COMP_int_en();
 }
 
 void timer2_prescaler_reset(){
     SFIOR |= (1<<PSR2);
+}
+/////////////////////////////////////////////////////////////////////////
+////////////////////////////timer1///////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+/**
+ * 
+ * @param wave  TIMER_NORMAL        
+ * 
+                TIMER_PWM_8BIT      
+                TIMER_PWM_9BIT      
+                TIMER_PWM_10BIT     
+
+                TIMER_CTC_OCR1A     
+
+                TIMER_FAST_PWM_8BIT 
+                TIMER_FAST_PWM_9BIT 
+                TIMER_FAST_PWM_10BIT
+
+                TIMER_PWM_FREQ_ICR1 
+                TIMER_PWM_FREQ_OCR1A
+
+                TIMER_PWM_ICR1      
+                TIMER_PWM_OCR1      
+
+                TIMER_CTC_ICR1      
+
+                TIMER_FAST_PWM_ICR1 
+                TIMER_FAST_PWM_OCR1     
+ * 
+ * @param clk   CLK_NO           
+                CLK              
+                CLK_8            
+                CLK_64           
+                CLK_256          
+                CLK_1024         
+                CLK_EX_FALLING   
+                CLK_EX_RISING
+ */
+void initTimer1(char wave, char clk){
+    //choose Waveform Generation Mode
+    timer1_selectMode(wave);
+
+    //choose Clock Select Bit Description
+    timer1_select_clock(clk);
+}
+void timer1_selectMode(char wave){
+    //reset them first
+    TCCR1A &= ~(1<<WGM11 | 1<<WGM10);
+    TCCR1B &= ~(1<<WGM13 | 1<<WGM12);
+                  //WGM13 WGM12   WGM11 WGM10
+    switch(wave){
+        case(TIMER_NORMAL)://00 00
+            break;
+            
+        case(TIMER_PWM_8BIT)://00 01
+            TCCR1A |= (1<<WGM10);
+            break;
+        case(TIMER_PWM_9BIT)://00 10
+            TCCR1A |= (1<<WGM11);
+            break;
+        case(TIMER_PWM_10BIT)://00 11
+            TCCR1A |= (1<<WGM11 | 1<<WGM10);
+            break;
+            
+        case(TIMER_CTC_OCR1A)://01 00
+            TCCR1B |= (1<<WGM12);
+            break;
+            
+        case(TIMER_FAST_PWM_8BIT)://01 01
+            TCCR1B |= (1<<WGM12);
+            TCCR1A |= (1<<WGM10);
+            break;
+        case(TIMER_FAST_PWM_9BIT)://01 10
+            TCCR1B |= (1<<WGM12);
+            TCCR1A |= (1<<WGM11);
+            break;
+        case(TIMER_FAST_PWM_10BIT)://01 11
+            TCCR1B |= (1<<WGM12);
+            TCCR1A |= (1<<WGM11 | 1<<WGM10);
+            break;
+            
+        case(TIMER_PWM_FREQ_ICR1)://10 00
+            TCCR1B |= (1<<WGM13);
+            break;
+        case(TIMER_PWM_FREQ_OCR1A)://10 01
+            TCCR1B |= (1<<WGM13);
+             TCCR1A |= (1<<WGM10);
+            break;
+            
+        case(TIMER_PWM_ICR1)://10 10
+            TCCR1B |= (1<<WGM13);
+            TCCR1A |= (1<<WGM11);
+            break;
+        case(TIMER_PWM_OCR1)://10 11
+            TCCR1B |= (1<<WGM13);
+            TCCR1A |= (1<<WGM11 | 1<<WGM10);
+            break;
+            
+        case(TIMER_CTC_ICR1)://11 00
+            TCCR1B |= (1<<WGM13 | 1<<WGM12);
+            break;
+                             //11 01 RESERVED
+        case(TIMER_FAST_PWM_ICR1)://11 10
+            TCCR1B |= (1<<WGM13 | 1<<WGM12);
+            TCCR1A |= (1<<WGM11);
+            break;
+        case(TIMER_FAST_PWM_OCR1)://11 11
+            TCCR1B |= (1<<WGM13 | 1<<WGM12);
+            TCCR1A |= (1<<WGM11 | 1<<WGM10);
+            break;
+    }
+}
+void timer1_select_clock(char clk){
+    TCCR1B &= ~((1<<CS10) | (1<<CS11) | (1<CS12)); //reset first
+    TCCR1B |= clk;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void timer1_TOV_int_en(){
+    TIMSK |= 1<<TOIE1;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void timer1A_COMP_int_en(){
+    TIMSK |= 1<<OCIE1A;
+}
+void timer1_OC1A_init(){
+    setPIND_dir(OC1A, OUT); //make sure we set it to output first;
+}
+/**
+ * 
+ * @param mode  OC1A_CTC_NORMAL      
+                OC1A_CTC_TOGGLE      
+                OC1A_CTC_CLEAR       
+                OC1A_CTC_SET         
+
+                OC1A_PWM_CLEAR_UP    
+                OC1A_PWM_SET_UP      
+
+                OC1A_FPWM_CLEAR      
+                OC1A_FPWM_SET        
+ */
+void timer1_OC1A_selectMode(char mode){
+    TCCR1A &= ~(1<<COM1A1 | 1<<COM1A0); //reset first
+    TCCR1A |= (mode<<COM1A0); //then OR the selected mode shifted by the place of the select mode bit
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void timer1B_COMP_int_en(){
+    TIMSK |= 1<<OCIE1B;
+}
+void timer1_OC1B_init(){
+    setPIND_dir(OC1B, OUT); //make sure we set it to output first;
+}
+/**
+ * 
+ * @param mode  OC1B_CTC_NORMAL      
+                OC1B_CTC_TOGGLE      
+                OC1B_CTC_CLEAR       
+                OC1B_CTC_SET         
+
+                OC1B_PWM_CLEAR_UP    
+                OC1B_PWM_SET_UP      
+
+                OC1B_FPWM_CLEAR      
+                OC1B_FPWM_SET        
+ */
+void timer1_OC1B_selectMode(char mode){
+    TCCR1A &= ~(1<<COM1B1 | 1<<COM1B0); //reset first
+    TCCR1A |= (mode<<COM1B0); //then OR the selected mode shifted by the place of the select mode bit
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void timer1_ICP_init(){
+    setPIND_dir(ICP1, IN); //make sure we set it to input first;
+    ACSR &= ~(1<<ACIC);
+}
+void timer1_ICP_int_en(){
+    TIMSK |= 1<<TICIE1;
+}
+void timer1_ICP_noise_filter(){
+    TCCR1B |= 1<<ICNC1;
+}
+/**
+ * 
+ * @param edge FALLING_EDGE
+ *             RISING_EDGE
+ */
+void timer1_ICP_selectEdge(char edge){
+    TCCR1B &= ~(1<<ICES1); //reset first
+    TCCR1B |= edge<<ICES1;
+}
+char timer1_ICP_getValue(){
+    return ICR1;
+}
+void timer1_ACO_init(){
+    setPINB_dir(AIN0, IN);
+    setPINB_dir(AIN1, IN);
+    ACSR |= (1<<ACIC);
 }
--- mtimer.h
+++ mtimer.h
@@ -8,6 +8,9 @@
 #ifndef MTIMER_H
 #define	MTIMER_H
 #include <avr/io.h>
+
+//timer 0
+
 //Waveform Generation Mode
 #define TIMER_NORMAL      0 //clear timer when it reaches max 255
 #define TIMER_PWM         1 //0 to 255 to 0
@@ -28,7 +31,7 @@
 #define OC0              PB3 //is output and can be any value (triggered when a compare match occurs)
 
 //type of output in OC0 after compare or overflow (according to wave and compare types)
-//o is disconnected for all
+//0 is disconnected for all
 #define OC0_DISCONNECTED          0
 //CTC
 #define OC0_CTC_TOGGLE            1 //Toggle OC0 on compare match
@@ -43,7 +46,6 @@
 #define OC0_FPWM_CLEAR      2 //Clear OC0 on compare match, set OC0 at BOTTOM
 #define OC0_FPWM_SET        3 //Set OC0 on compare match, clear OC0 at BOTTOM
 
-//timer 0
 void initTimer0(char wave, char clk);
 void timer0_selectMode(char wave);
 void timer0_select_clock(char clk);
@@ -54,6 +56,94 @@ void timer0_OC0_selectMode(char mode);
 void timer0_prescaler_reset();
 
 //timer 1
+
+//Waveform Generation Mode
+//to see when is the OCR1A and TOV1 flags are updated see data sheet page 114
+#define TIMER_NORMAL                0 //0 to FFFF
+
+#define TIMER_PWM_8BIT              1 //0 to 00FF to 0
+#define TIMER_PWM_9BIT              2 //0 to 01FF to 0
+#define TIMER_PWM_10BIT             3 //0 to 03FF to 0
+
+#define TIMER_CTC_OCR1A             4 //0 TO OCR1A
+
+#define TIMER_FAST_PWM_8BIT         5 //0 to 00FF
+#define TIMER_FAST_PWM_9BIT         6 //0 to 01FF
+#define TIMER_FAST_PWM_10BIT        7 //0 to 03FF
+
+#define TIMER_PWM_FREQ_ICR1         8 //0 TO ICR1
+#define TIMER_PWM_FREQ_OCR1A        9 //0 TO OCR1A
+
+#define TIMER_PWM_ICR1              10 //0 TO ICR1
+#define TIMER_PWM_OCR1              11 //0 TO OCR1A
+
+#define TIMER_CTC_ICR1              12 //0 TO ICR1
+
+#define TIMER_FAST_PWM_ICR1         13 //0 TO ICR1
+#define TIMER_FAST_PWM_OCR1         14 //0 TO OCR1A
+
+//type of output in OC1A/B after compare or overflow (according to wave and compare types)
+//0 is disconnected for all
+//A
+#define OC1A_DISCONNECTED          0
+//B
+#define OC1B_DISCONNECTED          0
+
+/////////////CTC/////////////
+//A
+#define OC1A_CTC_TOGGLE            1 //Toggle OC1A/B on compare match
+#define OC1A_CTC_CLEAR             2 //Clear OC1A/B on compare match
+#define OC1A_CTC_SET               3 //Set OC1A/B on compare match
+//B
+#define OC1B_CTC_TOGGLE            1 
+#define OC1B_CTC_CLEAR             2 
+#define OC1B_CTC_SET               3 
+
+//1 is reserved for both
+/////////////PWM/////////////
+//A
+#define OC1A_PWM_CLEAR_UP    2 //Clear OC1A/B on compare match when up-counting. Set OC1A/B on compare match when downcounting
+#define OC1A_PWM_SET_UP      3 //Set OC1A/B on compare match when up-counting. Clear OC1A/B on compare match when downcounting.
+//B
+#define OC1B_PWM_CLEAR_UP    2 
+#define OC1B_PWM_SET_UP      3 
+/////////////FAST PWM/////////////
+//A
+#define OC1A_FPWM_CLEAR      2 //Clear OC1A/B on compare match, set OC1A/B at BOTTOM
+#define OC1A_FPWM_SET        3 //Set OC1A/B on compare match, clear OC1A/B at BOTTOM
+//B
+#define OC1B_FPWM_CLEAR      2 
+#define OC1B_FPWM_SET        3 
+
+#define OC1A        PD5
+#define OC1B        PD4
+#define ICP1        PD6
+
+void initTimer1(char wave, char clk);
+void timer1_selectMode(char wave);
+void timer1_select_clock(char clk);
+
+void timer1_TOV_int_en(); //to enable Timer1 Overflow Interrupt
+
+void timer1A_COMP_int_en(); //to enable Timer1 A Output Compare Match Interrupt
+void timer1_OC1A_init();
+void timer1_OC1A_selectMode(char mode);
+
+void timer1B_COMP_int_en(); //to enable Timer1 B Output Compare Match Interrupt
+void timer1_OC1B_init();
+void timer1_OC1B_selectMode(char mode);
+
+void timer1_ICP_init();
+void timer1_ICP_int_en(); //to enable input capture interrupt
+void timer1_ICP_noise_filter(); //to enable nosie canceler
+#define FALLING_EDGE    0
+#define RISING_EDGE     1
+void timer1_ICP_selectEdge(char edge); //to select edge select mode
+char timer1_ICP_getValue();
+
+#define AIN0    PB2
+#define AIN1    PB3
+void timer1_ACO_init();
 
 
 //timer2
