dc437844c73e67e850a25ef1c8605bf6cfa6fb75
Youssef Omar the first <es-youssef.moh.ah2026@alexu.edu.eg>
2024-09-20 18:44:56 +0300

    FIXED SPI_TEST_WITH_EEPROM.c

--- COMMU protocals/SPI_TEST_WITH_EEPROM.c
+++ COMMU protocals/SPI_TEST_WITH_EEPROM.c
@@ -2,28 +2,19 @@
 #include "mHEADERS.h"
 #include "SPI_SLAVE_TEST.h"
 
-
-////define addresses for now (can change it later)
-//#define MYADDRESS       0x07
-////slave address  0x0A  -> 0000 1010
-//#define SLA_W   0x0A //write means LSB is zero
-//#define SLA_R   0x0B //read means LSB is one
-
-
 int main() {
     
     initBUTTON();
     initLCD4();
     initSPI(MASTER);
-    SPIclk(CLK_128);
-    
-    
-    //i have to write this first else it prints 'h' every time
-    EE_SEND_DATA(0xF0, 'z');
+    SPIclk(CLK_128);  
     
     char* str = "hello world";
     EE_SEND_DATA_STR(0x01, str);
-    
+
+    //must put delay to give time for SS to go back to HIGH
+    _delay_us(100);
+    EE_SEND_DATA(0xF0, 'z');
     
     while (1) {
         if (isPressed(BUTTON0))
